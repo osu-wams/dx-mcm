184d4016254bf2c1a7800302452db7992a6c161c
Josh Gum <revgum@gmail.com>
2020-03-25 07:47:53 -0700

    basic initial commit

---
+++ src/sender.ts
@@ -0,0 +1,52 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { SQS } from "aws-sdk";
+
+const sqs = new SQS();
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  let statusCode: number = 200;
+  let message: string;
+
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "No body was found"
+      })
+    };
+  }
+
+  const region = context.invokedFunctionArn.split(":")[3];
+  const accountId = context.invokedFunctionArn.split(":")[4];
+  const queueName: string = "receiverQueue";
+
+  const queueUrl: string = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
+
+  try {
+    await sqs
+      .sendMessage({
+        QueueUrl: queueUrl,
+        MessageBody: event.body,
+        MessageAttributes: {
+          AttributeNameHere: {
+            StringValue: "Attribute Value Here",
+            DataType: "String"
+          }
+        }
+      })
+      .promise();
+
+    message = "Message placed in the Queue!";
+  } catch (error) {
+    console.log(error);
+    message = error;
+    statusCode = 500;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message
+    })
+  };
+};
