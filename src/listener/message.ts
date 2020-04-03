4f9f9fa4bf67dddc3002b11d92edc0b6e4646689
Josh Gum <revgum@gmail.com>
2020-04-03 11:47:06 -0700

    adds message listener

---
+++ src/listener/message.ts
@@ -0,0 +1,25 @@
+import { SNSHandler } from 'aws-lambda';
+import Message from '../models/message';
+
+export const handler: SNSHandler = async (event, _context, _callback) => {
+  try {
+    for (const record of event.Records) {
+      const { content, contentShort, channelIds, populationParams } = JSON.parse(
+        record.Sns.Message,
+      );
+      console.log(JSON.parse(record.Sns.Message));
+      const message = await Message.upsert({
+        sendAt: new Date().toISOString().slice(0, 10),
+        id: new Date().toISOString().slice(-6),
+        status: 'NEW',
+        populationParams,
+        channelIds,
+        content,
+        contentShort,
+      });
+      console.log('Created -->  ', message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
