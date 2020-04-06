f9cee7e8e79323aaf22d86af1a1e5b40b97a268c
Josh Gum <revgum@gmail.com>
2020-04-06 08:55:31 -0700

    extracts dynamodb methods for consistent use and mocking

--- src/database.ts
+++ src/database.ts
@@ -4,4 +4,14 @@ import { DYNAMODB_API_VERSION } from './constants';
 import { DynamoDB } from 'aws-sdk';
 
 const database = new DynamoDB({ apiVersion: DYNAMODB_API_VERSION });
+
+export const putItem = (i: DynamoDB.PutItemInput): Promise<DynamoDB.PutItemOutput> =>
+  database.putItem(i).promise();
+
+export const query = (i: DynamoDB.QueryInput): Promise<DynamoDB.QueryOutput> =>
+  database.query(i).promise();
+
+export const createTable = (t: DynamoDB.CreateTableInput): Promise<DynamoDB.CreateTableOutput> =>
+  database.createTable(t).promise();
+
 export default database;
