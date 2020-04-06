7afa5095e69a451e9049d5b2e5961652c05c827c
Josh Gum <revgum@gmail.com>
2020-04-06 09:00:55 -0700

    ignore coverage for inappropriate files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/database', 'src/config'],
   moduleNameMapper: {
     '^@src(.*)$': '<rootDir>/src$1',
     '^@mocks(.*)$': '<rootDir>/mocks$1',
