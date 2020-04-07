81bedcaf9b49baa6b6fc0eaa1dcc7a681ce8004a
Josh Gum <revgum@gmail.com>
2020-04-07 11:54:16 -0700

    updates api gateway publisher to use api spec

--- src/api/publisher.ts
+++ src/api/publisher.ts
@@ -1,51 +1,87 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
 import { SNS } from 'aws-sdk';
 
-const sns = new SNS();
+export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  const { valid, response, payload, action } = validate(event);
+  if (!valid) return response;
 
-export const handler: APIGatewayProxyHandler = async (event, context) => {
-  if (!event.body) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'No body was found',
-      }),
-    };
-  }
   try {
-    console.log(context);
-    let statusCode: number = 200;
-    let message: string;
-    const execute = event.path.split('/').pop()?.toLowerCase();
-    const result = await sns
+    const sns = new SNS();
+    const published = await sns
       .publish({
-        Message: event.body,
+        Message: JSON.stringify(payload),
         TopicArn: process.env.SNS_TOPIC_ARN,
         MessageAttributes: {
-          execute: {
+          action: {
             DataType: 'String',
-            StringValue: execute,
+            StringValue: action,
           },
         },
       })
       .promise();
-
-    message = result.MessageId as string;
     return {
-      statusCode,
-      body: JSON.stringify({
-        message,
-        execute,
-      }),
+      statusCode: 200,
+      body: responseBody({ requestId: published.MessageId, action }),
     };
   } catch (error) {
-    console.log('boom');
     console.dir(error, { depth: null, showHidden: true });
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        error,
-      }),
+      body: responseBody({ error, action }),
     };
   }
+};
+interface IResponseBodyArgs {
+  error?: Error;
+  requestId?: string;
+  message?: string;
+  action: string | undefined;
+}
+
+const responseBody = ({ error, requestId, action, message }: IResponseBodyArgs): string => {
+  if (error) {
+    return JSON.stringify({
+      requestId,
+      message: error.message,
+      action,
+    });
+  }
+
+  return JSON.stringify({
+    requestId,
+    message,
+    action,
+  });
+};
+
+const validate = (event: APIGatewayProxyEvent) => {
+  const result = {
+    valid: false,
+    response: { statusCode: 400, body: JSON.stringify({ message: 'Bad Request' }) },
+    payload: {},
+    action: event.path.split('/').pop()?.toLowerCase(),
+  };
+
+  try {
+    if (!event.body) {
+      result.response.body = responseBody({ message: 'Missing data.', action: result.action });
+    } else {
+      const body = JSON.parse(event.body);
+      if (body.payload) {
+        result.valid = true;
+        result.response.statusCode = 200;
+        result.payload = body.payload;
+      } else {
+        result.response.body = responseBody({
+          message: 'Missing data payload.',
+          action: result.action,
+        });
+      }
+    }
+  } catch (error) {
+    result.response.statusCode = 500;
+    result.response.body = responseBody({ error, action: result.action });
+  }
+
+  return result;
 };
