a2befaa513bea76b9fdb671d6b28cd1cd2e6a7bb
Josh Gum <revgum@gmail.com>
2020-04-10 05:28:10 -0700

    add doc to method

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,11 @@
 import { SNSEvent, SNSEventRecord } from 'aws-lambda'; // eslint-disable-line no-unused-vars, import/no-unresolved
 
+/**
+ * Validate and return the SNS record associated to this event.
+ * @param event - An event from SNS
+ * @returns Array[boolean, record] - If the validation was successful and the record it validated or undefined
+ * @see <project_root>/events/lambda.sns.*
+ */
 export const validate = (event: SNSEvent): [boolean, SNSEventRecord | undefined] => {
   if (event.Records.length === 0) {
     console.error(`Event has no records -->  `, event);
