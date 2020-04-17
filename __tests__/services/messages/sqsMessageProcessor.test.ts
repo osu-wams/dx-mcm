664f2fa64d59d8ced0a126561e86b29537de482e
Josh Gum <revgum@gmail.com>
2020-04-17 11:34:40 -0700

    adds coverage for handler kicking off statemachine

--- __tests__/services/messages/sqsMessageProcessor.test.ts
+++ __tests__/services/messages/sqsMessageProcessor.test.ts
@@ -1,37 +1,21 @@
 import { handler } from '@src/services/messages/sqsMessageProcessor';
-// import { dynamoDbMessage } from '@mocks/message.mock';
 import * as event from '../../../events/lambda.sqs.messageProcessor.json';
 
-/*
-const mockCreateTable = jest.fn();
-const mockQuery = jest.fn();
-const mockPutItem = jest.fn();
-jest.mock('@src/database', () => ({
-  ...jest.requireActual('@src/database'),
-  createTable: () => mockCreateTable(),
-  query: () => mockQuery(),
-  putItem: () => mockPutItem(),
-}));
-*/
-
-const mockGetQueueUrl = jest.fn();
-const mockSendMessage = jest.fn();
-jest.mock('@src/messageQueue', () => ({
-  ...jest.requireActual('@src/messageQueue'),
-  sendMessage: () => mockSendMessage(),
-  getQueueUrl: () => mockGetQueueUrl(),
+const mockStartExecution = jest.fn();
+jest.mock('@src/stateMachine', () => ({
+  ...jest.requireActual('@src/stateMachine'),
+  startExecution: () => mockStartExecution(),
 }));
 
 describe('handler', () => {
   it('publishes pending messages to the queue', async () => {
-    mockGetQueueUrl.mockResolvedValue({ QueueUrl: 'some-url' });
+    mockStartExecution.mockResolvedValue({ executionArn: 'some-arn', startDate: '2020-01-01' });
     const result = await handler(event);
     expect(result).toEqual(undefined);
-    expect(mockGetQueueUrl).toHaveBeenCalled();
-    expect(mockSendMessage).toHaveBeenCalled();
+    expect(mockStartExecution).toHaveBeenCalled();
   });
   it('throws an error when there is a unhandled exception', async () => {
-    mockGetQueueUrl.mockRejectedValue('boom');
+    mockStartExecution.mockRejectedValue('boom');
     try {
       await handler(event);
     } catch (err) {
