434f2091a194d99840da8801bce33257e3c412e5
Josh Gum <revgum@gmail.com>
2020-04-17 11:29:48 -0700

    persists UserMessage to database and message queue for processing

--- src/services/messages/stepGetChannels.ts
+++ src/services/messages/stepGetChannels.ts
@@ -1,6 +1,6 @@
 export const handler = async (event: any, _context: any, callback: any) => {
   console.log(event);
-  callback(null, ['channel-1', 'channel-2']);
+  callback(null, { channels: ['channel-1', 'channel-2'] });
 };
 
 export default handler;
