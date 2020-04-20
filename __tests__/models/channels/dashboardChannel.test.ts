a569c0752203dd0647fa34dac630a622fa31a031
Josh Gum <revgum@gmail.com>
2020-04-20 11:33:19 -0700

    adds simple test coverage for the dashboard channel behavior

---
+++ __tests__/models/channels/dashboardChannel.test.ts
@@ -0,0 +1,20 @@
+import { userMessage, dynamoDbUserMessage } from '@mocks/userMessage.mock';
+import { getChannel } from '@src/models/userMessage';
+
+const mockQuery = jest.fn();
+const mockPutItem = jest.fn();
+jest.mock('@src/database', () => ({
+  ...jest.requireActual('@src/database'),
+  query: () => mockQuery(),
+  putItem: () => mockPutItem(),
+}));
+
+describe('dashboardChannel', () => {
+  it('processes a UserMessage', async () => {
+    mockQuery.mockResolvedValue({ Items: [dynamoDbUserMessage] });
+    const channel = getChannel(userMessage);
+    expect(await channel.process()).toBeUndefined();
+    expect(mockPutItem).toBeCalled();
+    expect(mockQuery).toBeCalled();
+  });
+});
