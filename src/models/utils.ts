cda823c3ccb5390792450cd96467020b33393460
Josh Gum <revgum@gmail.com>
2020-04-23 10:23:11 -0700

    adds method for url safe encoding and decoding of a complex object

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -16,4 +16,35 @@ export const applyMixins = (derivedCtor: any, baseCtors: any[]) => {
   });
 };
 
-export default applyMixins;
+// Map characters for url safe encoding
+const ENCODE: { [key: string]: string } = {
+  '+': '-',
+  '/': '_',
+  '=': '.',
+};
+
+// Map characters for decoding from url safe encoding
+const DECODE: { [key: string]: string } = {
+  '-': '+',
+  _: '/',
+  '.': '=',
+};
+
+/**
+ * Encode an object into url-safe base64 text
+ * @param object the object to encode as url-safe base64 text
+ */
+export const urlSafeBase64Encode = (object: Object): string => {
+  const base64 = Buffer.from(JSON.stringify(object)).toString('base64');
+  return base64.replace(/[+/=]/g, (c: any) => ENCODE[c]);
+};
+
+/**
+ * Decode and return the object represented by url-safe base64 encoded text
+ * @param encoded the url-safe base64 encoded text to decode
+ */
+export const urlSafeBase64Decode = (encoded: string): Object => {
+  const base64 = encoded.replace(/[-_.]/g, (c: any) => DECODE[c]);
+  const ascii = Buffer.from(base64, 'base64').toString('ascii');
+  return JSON.parse(ascii);
+};
