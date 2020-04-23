150f1a5c207171bd28d39181794c66793b296961
Josh Gum <revgum@gmail.com>
2020-04-23 11:50:53 -0700

    uses lastKey if it exists

--- src/services/userMessages/httpUserMessagesList.ts
+++ src/services/userMessages/httpUserMessagesList.ts
@@ -1,17 +1,17 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'; // eslint-disable-line no-unused-vars, import/no-unresolved
-import UserMessage from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
+import UserMessage, { UserMessageResults } from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
-    const { osuId } = event.pathParameters ?? { osuId: undefined };
+    const { osuId, lastKey } = event.pathParameters ?? { osuId: undefined, lastKey: undefined };
     if (!osuId) throw new Error('Missing osuId in path.');
 
-    const userMessages: UserMessage[] | null = await UserMessage.findAll(osuId);
-    if (!userMessages) throw new Error('No user messages found.');
+    const userMessageResults: UserMessageResults = await UserMessage.findAll(osuId, lastKey);
+    if (!userMessageResults.count) throw new Error('No user messages found.');
 
     return {
       statusCode: 200,
-      body: JSON.stringify(userMessages),
+      body: JSON.stringify(userMessageResults),
     };
   } catch (error) {
     console.dir(error, { depth: null, showHidden: true });
