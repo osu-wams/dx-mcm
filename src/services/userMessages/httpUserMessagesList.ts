19502cd92d01984fd19999b9dda906d09c320c16
Josh Gum <revgum@gmail.com>
2020-04-28 08:49:32 -0700

    converts to use responseBody http util for standardizing responses

--- src/services/userMessages/httpUserMessagesList.ts
+++ src/services/userMessages/httpUserMessagesList.ts
@@ -1,7 +1,9 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'; // eslint-disable-line no-unused-vars, import/no-unresolved
+import { responseBody } from '@src/services/httpUtils';
 import UserMessage, { UserMessageResults, ChannelId } from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
 
 export const handler = async (event: APIGatewayProxyEvent) => {
+  const action = 'userMessages-list';
   try {
     const { osuId, lastKey, channelId } = event.pathParameters ?? {
       osuId: undefined,
@@ -9,27 +11,28 @@ export const handler = async (event: APIGatewayProxyEvent) => {
       channelId: undefined,
     };
     if (!osuId) throw new Error('Missing osuId in path.');
+
     let userMessageResults: UserMessageResults;
     if (channelId) {
       const selectedChannel = ChannelId[channelId.toUpperCase() as keyof typeof ChannelId];
       if (!selectedChannel) throw new Error('Missing valid channelId in path.');
-
       userMessageResults = await UserMessage.byChannel(osuId, selectedChannel, lastKey);
     } else {
       userMessageResults = await UserMessage.findAll(osuId, lastKey);
     }
 
-    if (!userMessageResults.count) throw new Error('No user messages found.');
-
     return {
       statusCode: 200,
-      body: JSON.stringify(userMessageResults),
+      body: responseBody({
+        action,
+        object: { userMessageResults },
+      }),
     };
   } catch (error) {
     console.dir(error, { depth: null, showHidden: true });
     return {
       statusCode: 500,
-      body: JSON.stringify({ error }),
+      body: responseBody({ error, action }),
     };
   }
 };
