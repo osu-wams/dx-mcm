7ccc4b33e6509c4f28d10e119ae5953ce57889d1
Josh Gum <revgum@gmail.com>
2020-05-21 06:58:57 -0700

    adds usermessage API query for all by a status code

--- src/services/userMessages/httpUserMessagesList.ts
+++ src/services/userMessages/httpUserMessagesList.ts
@@ -1,24 +1,29 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'; // eslint-disable-line no-unused-vars, import/no-unresolved
 import { responseBody, successResponse, errorResponse } from '@src/services/httpUtils';
-import UserMessage, { UserMessageResults, ChannelId } from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
+import UserMessage, { UserMessageResults, ChannelId, Status } from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   const action = 'userMessages-list';
   try {
-    const { osuId, lastKey, channelId } = event.pathParameters ?? {
+    const { osuId, lastKey, channelId, status } = event.pathParameters ?? {
       osuId: undefined,
       lastKey: undefined,
       channelId: undefined,
+      status: undefined,
     };
-    if (!osuId) throw new Error('Missing osuId in path.');
+    if (!osuId && !status) throw new Error('Missing osuId in path.');
 
     let userMessageResults: UserMessageResults;
     if (channelId) {
       const selectedChannel = ChannelId[channelId.toUpperCase() as keyof typeof ChannelId];
       if (!selectedChannel) throw new Error('Missing valid channelId in path.');
-      userMessageResults = await UserMessage.byChannel(osuId, selectedChannel, lastKey);
+      userMessageResults = await UserMessage.byChannel(osuId!, selectedChannel, lastKey);
+    } else if (status) {
+      const selectedStatus = Status[status.toUpperCase() as keyof typeof Status];
+      if (!selectedStatus) throw new Error('Missing valid status in path.');
+      userMessageResults = await UserMessage.allByStatus(selectedStatus, lastKey);
     } else {
-      userMessageResults = await UserMessage.findAll(osuId, lastKey);
+      userMessageResults = await UserMessage.findAll(osuId!, lastKey);
     }
 
     return successResponse({
