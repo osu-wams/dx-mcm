ef94b92da97d2fe355ca88ce216a4450b89d527e
Josh Gum <revgum@gmail.com>
2020-09-16 09:42:54 -0700

    fixes tests and adds coverage to message publisher

--- __tests__/services/messages/httpMessagesPublisher.test.ts
+++ __tests__/services/messages/httpMessagesPublisher.test.ts
@@ -1,19 +1,52 @@
 import { handler } from '@src/services/messages/httpMessagesPublisher';
+import { message } from '@mocks/message.mock';
 import * as event from '../../../events/lambda.http.messagesPublisher.json';
 
 const mockEvent = jest.fn();
-const mockPublishToQueue = jest.fn();
+const mockGetQueueUrl = jest.fn();
+const mockSendMessage = jest.fn();
 jest.mock('@src/messageQueue', () => ({
   // @ts-ignore
   ...jest.requireActual('@src/messageQueue'),
-  publishToQueue: () => mockPublishToQueue(),
+  sendMessage: () => mockSendMessage(),
+  getQueueUrl: () => mockGetQueueUrl(),
+}));
+
+const mockQuery = jest.fn();
+const mockPutItem = jest.fn();
+jest.mock('@src/database', () => ({
+  // @ts-ignore
+  ...jest.requireActual('@src/database'),
+  createTable: () => jest.fn(),
+  query: () => mockQuery(),
+  putItem: () => mockPutItem(),
 }));
 
 beforeEach(() => {
   mockEvent.mockReturnValue(event);
+  mockGetQueueUrl.mockResolvedValue({ QueueUrl: 'some-url' });
+  mockSendMessage.mockResolvedValue(true);
+  mockQuery
+    .mockResolvedValueOnce({ Items: [], Count: 0 })
+    .mockResolvedValueOnce({ Items: [message], Count: 1 });
+  mockPutItem.mockResolvedValue(message);
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('handler', () => {
+  // TODO: figure out how mock Query should return multiple values in succession
+  it('creates a new message', async () => {
+    mockEvent.mockReturnValue({
+      ...mockEvent(),
+    });
+    const result = await handler(mockEvent());
+    expect(result).toMatchObject({ statusCode: 200 });
+    expect(mockGetQueueUrl).toHaveBeenCalled();
+    expect(mockSendMessage).toHaveBeenCalled();
+  });
   it('fails to validate request', async () => {
     mockEvent.mockReturnValue({
       ...mockEvent(),
@@ -21,14 +54,15 @@ describe('handler', () => {
     });
     const result = await handler(mockEvent());
     expect(result).toMatchObject({ statusCode: 400 });
-    expect(mockPublishToQueue).not.toHaveBeenCalled();
+    expect(mockSendMessage).not.toHaveBeenCalled();
   });
   it('fails to publish a message to the queue', async () => {
+    mockSendMessage.mockRejectedValue(false);
     const result = await handler(mockEvent());
     expect(result).toMatchObject({ statusCode: 500 });
   });
   it('throws an error when there is a unhandled exception', async () => {
-    mockPublishToQueue.mockRejectedValue('boom');
+    mockSendMessage.mockRejectedValue('boom');
     try {
       await handler(mockEvent());
     } catch (err) {
