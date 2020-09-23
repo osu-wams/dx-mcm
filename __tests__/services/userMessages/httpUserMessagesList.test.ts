51942aa04d2f3e086b7fded373129d6c5356e1f1
Josh Gum <revgum@gmail.com>
2020-09-23 14:12:23 -0700

    removes tests for old api functions

--- __tests__/services/userMessages/httpUserMessagesList.test.ts
+++ __tests__/services/userMessages/httpUserMessagesList.test.ts
@@ -1,8 +1,6 @@
 import { handler } from '@src/services/userMessages/httpUserMessagesList';
 import { dynamoDbUserMessage, userMessage } from '@mocks/userMessage.mock';
-import * as event from '../../../events/lambda.http.userMessagesList.json';
 import * as channelEvent from '../../../events/lambda.http.userMessagesListByChannelId.json';
-import * as statusEvent from '../../../events/lambda.http.userMessagesListByStatus.json';
 
 const mockEvent = jest.fn();
 const mockQuery = jest.fn();
@@ -12,45 +10,12 @@ jest.mock('@src/database', () => ({
   query: () => mockQuery(),
 }));
 
-beforeEach(() => {
-  mockEvent.mockReturnValue(event);
-});
-
 describe('handler', () => {
-  it('fetches user messages for a specific user', async () => {
-    mockQuery.mockResolvedValue({ Items: [dynamoDbUserMessage], Count: 1 });
-    mockEvent.mockReturnValue({ ...event, pathParameters: { userId: 'bobross-111111111' } });
-    const result = await handler(mockEvent());
-    expect({ ...result, body: JSON.parse(result.body) }).toMatchObject({
-      body: {
-        action: 'userMessages-list',
-        object: { userMessageResults: { items: [userMessage], count: 1 } },
-      },
-      statusCode: 200,
-    });
-    expect(mockQuery).toHaveBeenCalled();
-  });
   it('fetches user messages on a channel for a specific user', async () => {
     mockQuery.mockResolvedValue({ Items: [dynamoDbUserMessage], Count: 1 });
     mockEvent.mockReturnValue({
       ...channelEvent,
       pathParameters: { userId: 'bobross-111111111', channelId: 'dashboard' },
-    });
-    const result = await handler(mockEvent());
-    expect({ ...result, body: JSON.parse(result.body) }).toMatchObject({
-      body: {
-        action: 'userMessages-list',
-        object: { userMessageResults: { items: [userMessage], count: 1 } },
-      },
-      statusCode: 200,
-    });
-    expect(mockQuery).toHaveBeenCalled();
-  });
-  it('fetches user messages in a particular status', async () => {
-    mockQuery.mockResolvedValue({ Items: [dynamoDbUserMessage], Count: 1 });
-    mockEvent.mockReturnValue({
-      ...statusEvent,
-      pathParameters: { status: 'error' },
     });
     const result = await handler(mockEvent());
     expect({ ...result, body: JSON.parse(result.body) }).toMatchObject({
@@ -70,17 +35,6 @@ describe('handler', () => {
     const result = await handler(mockEvent());
     expect({ ...result, body: JSON.parse(result.body) }).toMatchObject({
       body: { action: 'userMessages-list', message: 'Missing valid channelId in path.' },
-      statusCode: 500,
-    });
-  });
-  it('should respond with an error when an invalid status is provided', async () => {
-    mockEvent.mockReturnValue({
-      ...statusEvent,
-      pathParameters: { status: 'invalid-status' },
-    });
-    const result = await handler(mockEvent());
-    expect({ ...result, body: JSON.parse(result.body) }).toMatchObject({
-      body: { action: 'userMessages-list', message: 'Missing valid status in path.' },
       statusCode: 500,
     });
   });
