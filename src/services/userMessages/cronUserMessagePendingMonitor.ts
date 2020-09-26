51a67d9445bbb57de421619feba469893bc0d051
Josh Gum <revgum@gmail.com>
2020-09-26 14:03:24 -0700

    implements a basic cron for monitoring usermessages in error state and making slack notifications

---
+++ src/services/userMessages/cronUserMessagePendingMonitor.ts
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import { Status } from '@src/models/message';
+import { DX_ALERTS_SLACK_HOOK, ENV, USER_MESSAGE_API_URL } from '@src/constants';
+import UserMessagePending from '@src/models/userMessagePending';
+
+const fetchErrors = async (pendingMinAgo: number): Promise<UserMessagePending[]> => {
+  const items: UserMessagePending[] = [];
+  let lastKey: string | undefined;
+  do {
+    // eslint-disable-next-line no-await-in-loop
+    const results = await UserMessagePending.updatedSince(Status.ERROR, pendingMinAgo, lastKey);
+    if (results.lastKey) lastKey = results.lastKey;
+    items.push(...results.items);
+  } while (lastKey);
+
+  items.forEach(({ id, channelMessageId, status, updatedAt, sendAt, statusMessage }) =>
+    console.error(`UserMessage: (${id} / ${channelMessageId}) is in ${status} state.`, {
+      id,
+      channelMessageId,
+      sendAt,
+      status,
+      updatedAt,
+      statusMessage,
+    }),
+  );
+  return items;
+};
+
+const getStatusMessages = (items: UserMessagePending[]): (string | undefined)[] => {
+  return [...new Set(items.map((i) => i.statusMessage))];
+};
+
+// eslint-disable-next-line no-unused-vars
+export const handler = async (_event: any, _context: any, _callback: any) => {
+  const pendingMinAgo = parseInt((process.env.PENDING_MIN_AGO ?? '1').toString(), 10);
+  const fromDate = new Date(Date.now() - pendingMinAgo).toISOString().slice(0, 16);
+  const items = await fetchErrors(pendingMinAgo);
+
+  if (items.length) {
+    const divider = { type: 'divider' };
+    const title = {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*MCM UserMessage Monitor* : ${ENV} since ${fromDate}\nErrors: ${items.length}`,
+      },
+    };
+    const data: { blocks: { type: string; text?: { type: string; text: string } }[] } = {
+      blocks: [title],
+    };
+
+    getStatusMessages(items).forEach((s) => {
+      const text = items
+        .filter((i) => i.statusMessage === s)
+        .map(
+          (i) =>
+            `${i.messageChannelUser} : <${USER_MESSAGE_API_URL}/error/${encodeURIComponent(
+              i.messageChannelUser,
+            )}|view> | <${USER_MESSAGE_API_URL}/error/${encodeURIComponent(
+              i.messageChannelUser,
+            )}/retry|retry>`,
+        )
+        .join('\n');
+      const itemDetail = { type: 'section', text: { type: 'mrkdwn', text } };
+      const context = {
+        type: 'context',
+        elements: [{ type: 'mrkdwn', text: `:memo: Error: ${s}` }],
+      };
+      data.blocks.push(divider, context, itemDetail);
+    });
+
+    await axios.post(DX_ALERTS_SLACK_HOOK, data, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+};
+
+export default handler;
