9d5111325d6968a607064a0564b83e2a62995aaf
Josh Gum <revgum@gmail.com>
2020-09-29 15:15:56 -0700

    refactor SQS publish method

--- src/services/sqsUtils.ts
+++ src/services/sqsUtils.ts
@@ -3,6 +3,11 @@ import { AWSError, SQS } from 'aws-sdk'; // eslint-disable-line no-unused-vars
 import { getQueueUrl as messageQueueGetQueueUrl, sendMessage } from '@src/messageQueue';
 import type Message from '@src/models/message'; // eslint-disable-line no-unused-vars
 import type UserMessage from '@src/models/userMessage'; // eslint-disable-line no-unused-vars
+import {
+  SQS_ERROR_USER_MESSAGE_QUEUE_NAME,
+  SQS_PROCESS_USER_MESSAGE_QUEUE_NAME,
+} from '@src/constants';
+import throat from 'throat';
 
 /**
  * Validate and return the SQS records associated to this event.
@@ -53,4 +58,34 @@ export const publishToQueue = async (
     console.error('Failed to publish -->  ', message, published);
   }
   console.log('Published -->  ', queueUrl, message, published);
+};
+
+/**
+ * Publish each UserMessages instance to the processing queue where a lambda will
+ * fire off processing each individual UserMessage and deliver it to its intended channel. If
+ * publishing to the processing queue fails, then publish to the error queue for disposition.
+ * @param userMessages the UserMessage instances that were persisted to DynamoDB
+ */
+export const publishUserMessagesToQueue = async (userMessages: UserMessage[]): Promise<void[]> => {
+  try {
+    const queueUrl = await getQueueUrl(SQS_PROCESS_USER_MESSAGE_QUEUE_NAME);
+    const errorQueueUrl = await getQueueUrl(SQS_ERROR_USER_MESSAGE_QUEUE_NAME);
+    return Promise.all(
+      userMessages.map(
+        throat(50, (userMessage: UserMessage) => {
+          return publishToQueue(userMessage!, queueUrl).catch((err) => {
+            // TODO: Mark the UserMessage as a fail status?
+            publishToQueue({ object: { userMessage }, error: err }, errorQueueUrl);
+            console.error(err);
+          });
+        }),
+      ),
+    );
+  } catch (err) {
+    // Throw error caught when one of the getQueueUrl fails, setting Message in error state
+    console.error(err);
+    throw new Error(
+      `${userMessages.length} UserMessages persisted/upserted in DynamoDB, but publishing to SQS failed. Caught: ${err}`,
+    );
+  }
 };
