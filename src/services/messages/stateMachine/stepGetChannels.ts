bab57c1237af1f8650f9ad46fe86705283af22d9
Josh Gum <revgum@gmail.com>
2020-11-16 12:00:29 -0800

    converge on async/await not mixed buggy callback

--- src/services/messages/stateMachine/stepGetChannels.ts
+++ src/services/messages/stateMachine/stepGetChannels.ts
@@ -1,7 +1,8 @@
-export const handler = async (event: any, _context: any, callback: any) => {
+// eslint-disable-next-line
+export const handler = async (event: any, _context: any) => {
   // Placeholder for looking up or validating channelIds, for now the application
   // will pass the data through.
-  callback(null, { channels: event.channelIds });
+  return { channels: event.channelIds };
 };
 
 export default handler;
